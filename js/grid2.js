4a0fd802c46540ae3722be135ccbecf352e48d8a
Ananya Ganesh <ananyaganesh@Ananyas-MacBook-Pro.local>
2022-11-30 16:43:07 +0530

    subject

--- js/grid2.js
+++ js/grid2.js
@@ -1,67 +1,66 @@
-let Hslider, Vslider, RdShape, CpShape1, CpShape2 ;
+let Hslider, Bslider, RdShape, CpShape1, Heightslider, WidthSlider, RadiusSlider;
 
-function setup()
-{
-  createCanvas(600, 600);
-  Hslider = createSlider(1 , 9, 8)
-  Hslider.position(610, 25)
-  Hslider.style('width', '160px')
- 
- 
-  Vslider = createSlider(1 , 9, 8)
-  Vslider.position(610, 50)
-  Vslider.style('width', '160px')
- 
+function setup() {
+  createCanvas(400, 400);
+  Hslider = createSlider(1,10,1);
+  Hslider.position(500, 500);
+  Hslider.style('width', '160px');
+  
+  Bslider = createSlider(1,10,1);
+  Bslider.position(500, 550);
+  Bslider.style('width', '160px');
+  
+  Heightslider = createSlider(1,10,1);
+  Heightslider.position(500, 450);
+  Heightslider.style('width', '160px');
+  
+  WidthSlider = createSlider(1,10,1);
+  WidthSlider.position(500, 400);
+  WidthSlider.style('width', '160px');
+  
+  RadiusSlider = createSlider(1,10,1);
+  RadiusSlider.position(500, 350);
+  RadiusSlider.style('width', '160px');
+  
   RdShape = createRadio();
-  RdShape.option("Rect")
-  RdShape.option("RoundRect")
+  RdShape.option("Rectangle")
   RdShape.option("Circle")
-  RdShape.selected("Rect")
-  RdShape.position(610, 75)
- 
-  CpShape1 = createColorPicker('#e1136e')
-  CpShape1.position(610, 120)
+  RdShape.selected("Rectangle")
+  RdShape.option("Rounded Rectangle")
+  RdShape.position(500, 600)
+  
+  CpShape1 = createColorPicker("#ed225d")
+  CpShape1.position(500,650)
   CpShape1.style("height", "25px")
- 
-  CpShape2 = createColorPicker('#f0b825')
-  CpShape2.position(690, 120)
+  
+  CpShape2 = createColorPicker("#ed3d")
+  CpShape2.position(550,650)
   CpShape2.style("height", "25px")
- 
 }
 
-function draw()
-{
+function draw() {
   background(220);
-
-  for(m=0 ; m < Vslider.value() ; m++)
-    {
-      for (i=0; i < Hslider.value() ; i++)
+  
+  for(j=0;j<Bslider.value();j++){
+    for (i=0; i<Hslider.value(); i++){
+        if(i%2==0 && j%2==0)
+          {
+            fill(CpShape1.color())
+          }
+      else if(i%2==1 && j%2==1)
         {
-          if(i%2==1 && m%2==1 )
-            {
-              fill(CpShape1.color())
-            }
-          else if(i%2==0 && m%2==0 )
-            {
-              fill(CpShape1.color())
-            }
-          else
-            {
-              fill(CpShape2.color())
-            }
-         
-          if (RdShape.value() == "Rect")
-            {
-              rect( 60*i+20, 60*m, 50 , 50 )
-            }
-          else if (RdShape.value() == "RoundRect")
-            {
-              rect( 60*i+20, 60*m, 50 , 50 , 10 )
-            }
-          else
-            {
-              circle ( 60*i+20, 60*m, 50 )
-            }    
+      fill(CpShape1.color())
         }
+      else 
+        {
+          fill(CpShape2.color())
+        }
+      if (RdShape.value() == "Rectangle")
+        rect(i * 40 + 2, j * 40+1, Heightslider.value()+20, WidthSlider.value()+20);
+      else if (RdShape.value() == "Rounded Rectangle")
+        rect(i * 40 + 2, j * 40+1, Heightslider.value()+20, WidthSlider.value()+20, 10);
+      else 
+        circle(i * 40 + 20, j * 40+20, RadiusSlider.value()+20)
+      }
     }
 }
